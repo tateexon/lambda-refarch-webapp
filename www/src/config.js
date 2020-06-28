0d69b1ad0ee0ebc039d55d2ffac3980c1b99a16b
Tate Exon <craztad@gmail.com>
2020-06-27 20:21:27 -0600

    Frontend config update

--- www/src/config.js
+++ www/src/config.js
@@ -2,12 +2,12 @@
 // aws cloudformation describe-stacks --stack-name <YOUR STACK NAME> --query "Stacks[0].Outputs[]"
 
 const config = {
-  "aws_cognito_region": "us-east-1",
-  "aws_user_pools_id": "None",                           // CognitoID
-  "aws_user_pools_web_client_id": "None",     // CognitoClientID
-  "api_base_url": "None",                                     // TodoFunctionApi
-  "coginto_hosted_domain": "None",                   // CognitoDomainName
-  "redirect_url": "None"                                      // AmplifyURL
+  "aws_cognito_region": "us-west-2",
+  "aws_user_pools_id": "",                           // CognitoID
+  "aws_user_pools_web_client_id": "",     // CognitoClientID
+  "api_base_url": "",                                     // TodoFunctionApi
+  "coginto_hosted_domain": "",                   // CognitoDomainName
+  "redirect_url": ""                                      // AmplifyURL
 };
 
 export default config;
